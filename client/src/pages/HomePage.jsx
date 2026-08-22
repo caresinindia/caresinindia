5f380031bda8db17fc6c3154e1e8b79695538bc6
Prince Kumar <221290926+priince-69@users.noreply.github.com>
2026-08-22 13:03:33 +0530

    testing the git

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -14,4 +14,17 @@ function HomePage() {
   );
 }
 
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8" />
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//   <title>Document</title>
+// </head>
+// <body>
+  
+// </body>
+// </html>
+
 export default HomePage;
